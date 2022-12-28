9994304449e6c0fed6997f12b089e6212e74cd6e
Charalambos Georgiades <CGeorgiades1@sheffield.ac.uk>
2022-12-28 02:11:27 +0000

    Added a few comments

--- get_images_from_web.py
+++ get_images_from_web.py
@@ -3,10 +3,12 @@ import simple_image_download.simple_image_download as simp
 # Download images from the web
 response = simp.Downloader()
 
+# Change the directory to save the images and the format
 response.directory = "web_images/"
 response.extensions = ".jpg"
 
-keywords = ["flashpoint batman"]
+# Keywords to search for
+keywords = ["bear riding a shark", "elf on a shelf"]
 
 for kw in keywords:
     response.download(kw.replace(" ", "+"), limit = 10)
