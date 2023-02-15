5143cb90d22ef24e6b36917891eaaa74429b5057
Charalambos Georgiades <CGeorgiades1@sheffield.ac.uk>
2023-02-15 04:26:43 +0000

    Can choose which person a face belongs to

--- tracker.py
+++ tracker.py
@@ -33,12 +33,18 @@ def remove_suffix(image):
 
     return image
 
-def draw_on_image(temp_img, x, y, w, h, folder):
+def draw_on_image(temp_img, x, y, w, h, labels):
+
+    space_between_lines = 30
 
     cv2.rectangle(temp_img, (x, y),(x + w, y + h),(0, 255, 255), 2)
-    cv2.putText(temp_img, folder.replace('+',' '), (x, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-    cv2.putText(temp_img, "Q - Discard Box", (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
-    cv2.putText(temp_img, "P - Keep Box", (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
+    cv2.putText(temp_img, "Face", (x, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
+    for i in range(len(labels)):
+        cv2.putText(temp_img, str(i + 1) + " - " + labels[i], (10, space_between_lines), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
+        space_between_lines += 30
+
+    cv2.putText(temp_img, "Else - Discard", (10, space_between_lines), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
 
 # Paths
 folder_path = "results/"
@@ -66,6 +72,10 @@ Path(val_folder_labels).mkdir(parents=True, exist_ok=True)
 Path(test_folder_images).mkdir(parents=True, exist_ok=True)
 Path(test_folder_labels).mkdir(parents=True, exist_ok=True)
 
+# Labels
+
+labels = ['Walter White', 'Jessie Pinkman', 'Tuco Salamanca']
+
 # Read haar cascades for detection
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt.xml')
 face_cascade2 = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
@@ -73,18 +83,13 @@ face_cascade2 = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt
 # Get list of subfolders 
 folders = os.listdir(folder_path)
 
-class_counter = 0
-total_images = 0
-
 for folder in folders:
     
-    subfolder_path = folder_path + folder
+    subfolder_path = os.path.join(folder_path, folder)
     # Get list of images in subfolder
     images = os.listdir(subfolder_path)
 
     number_of_images = len(images)
-
-    total_images += number_of_images
 
     threshold_for_training = int(number_of_images * 0.8)
     threshold_for_validation = int(number_of_images * 0.9)
@@ -95,36 +100,36 @@ for folder in folders:
     
         # make sure file is an image
         if image.endswith(('.jpg', '.png', 'jpeg', '.JPG', '.PNG', 'JPEG')):
-            img_path = subfolder_path + "/" + image
+            img_path = os.path.join(subfolder_path, image) 
 
             threshold_counter += 1
 
             if threshold_counter > threshold_for_validation:
 
                 # Copy image to validation folder
-                shutil.copyfile(img_path, test_folder + "/" + images_path + "/" + image)
-                img_path = test_folder + "/" + images_path + "/" + image
+                shutil.copyfile(img_path, os.path.join(test_folder_images, image))
+                img_path = os.path.join(test_folder_images, image)
                 # Remove file extension
                 image = remove_suffix(image)
-                label_file = open(test_folder + "/" + labels_path + "/" + image + ".txt", "w")
+                label_file = open(os.path.join(test_folder_labels, image ) + ".txt", "w")
 
             elif threshold_counter > threshold_for_training:
                 
                 # Copy image to validation folder
-                shutil.copyfile(img_path, val_folder + "/" + images_path + "/" + image)
-                img_path = val_folder + "/" + images_path + "/" + image
+                shutil.copyfile(img_path, os.path.join(val_folder_images, image))
+                img_path = os.path.join(val_folder_images, image)
                 # Remove file extension
                 image = remove_suffix(image)
-                label_file = open(val_folder + "/" + labels_path + "/" + image + ".txt", "w")
+                label_file = open(os.path.join(val_folder_labels, image) + ".txt", "w")
             
             else:
 
                 # Copy image to training folder
-                shutil.copyfile(img_path, train_folder + "/" + images_path + "/" + image)
-                img_path = train_folder + "/" + images_path + "/" + image
+                shutil.copyfile(img_path, os.path.join(train_folder_images, image))
+                img_path = os.path.join(train_folder_images, image)
                 # Remove file extension
                 image = remove_suffix(image)
-                label_file = open(train_folder + "/" + labels_path + "/" + image + ".txt", "w")
+                label_file = open(os.path.join(train_folder_labels, image) + ".txt", "w")
 
             # Read input image
             img = cv2.imread(img_path)
@@ -136,8 +141,8 @@ for folder in folders:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             # Detects faces in the input image
-            faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors = 7)
-            faces2 = face_cascade2.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors =  7)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 3)
+            faces2 = face_cascade2.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors =  3)
 
             if len(faces) != 0 and len(faces2) != 0:
                 faces = np.concatenate((faces, faces2))
@@ -162,29 +167,26 @@ for folder in folders:
                 width_norm, height_norm, x_center_norm, y_center_norm = opencv_to_yolo(img_width, img_height, x, y, w, h)
 
                 # Draw a rectangle in a face to check accuracy of conversion
-                draw_on_image(temp_img, x, y, w, h, folder)
+                draw_on_image(temp_img, x, y, w, h, labels)
 
                 # Display the image in a window
                 cv2.imshow(image, temp_img)
-    
-                if cv2.waitKey(0) == ord('q'):
+
+                key = cv2.waitKey(0)
+
+                found = False
+
+                for i in range(0, len(labels)):
+                    if key == ord(str(i+1)):
+                        print(labels[i])
+                        label_file.writelines(str(i) + " " + str(x_center_norm) + " " + str(y_center_norm) + " " + str(width_norm) + " " + str(height_norm) + "\n")
+                        cv2.destroyAllWindows()
+                        found = True
+                        break
+
+                if not found:
                     print("Discard")
                     cv2.destroyAllWindows()
-                    continue
-                else:
-                    print("Keep")
-                    label_file.writelines(str(class_counter) + " " + str(x_center_norm) + " " + str(y_center_norm) + " " + str(width_norm) + " " + str(height_norm) + "\n")
-                    cv2.destroyAllWindows()
-
-
-                # Possible solution to the above problem
-                # k = cv2.waitKey(0)
-                # if k==27:    # Esc key to stop
-                #     break
-                # elif k==-1:  # normally -1 returned,so don't print it
-                #     continue
-                # else:
-                #     print k # else print its value
 
         label_file.close()
 
@@ -192,8 +194,6 @@ for folder in folders:
         if os.stat(label_file.name).st_size == 0:
             os.remove(img_path)
             os.remove(label_file.name)
-
-    class_counter += 1
 
 cv2.destroyAllWindows()
 
